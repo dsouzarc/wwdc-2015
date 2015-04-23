ef17d008d91ae0fdbc7a4c5e34524d1bba6728fd
Ryan D'souza <dsouzarc@gmail.com>
2015-04-23 17:16:24 -0400

    First commit

---
+++ WWDC Scholarship 2015/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WWDC Scholarship 2015
+//
+//  Created by Ryan D'souza on 4/23/15.
+//  Copyright (c) 2015 Ryan D'souza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WWDC Scholarship 2015/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WWDC Scholarship 2015
+//
+//  Created by Ryan D'souza on 4/23/15.
+//  Copyright (c) 2015 Ryan D'souza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
