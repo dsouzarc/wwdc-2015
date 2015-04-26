e609d1c309f4a0a097c5e0926528382ea9948529
Ryan D'souza <dsouzarc@gmail.com>
2015-04-26 15:26:57 -0400

    Name, profile picture, coverphoto

--- WWDC Scholarship 2015/CenterAboutMeViewController.m
+++ WWDC Scholarship 2015/CenterAboutMeViewController.m
@@ -10,28 +10,21 @@
 
 @interface CenterAboutMeViewController ()
 
+@property (strong, nonatomic) IBOutlet UILabel *topNameTitle;
+@property (strong, nonatomic) IBOutlet UIImageView *backgroundCoverphoto;
+
+@property (strong, nonatomic) IBOutlet UIImageView *profilePictureView;
 @end
 
 @implementation CenterAboutMeViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
-}
+    
+    self.profilePictureView.layer.cornerRadius = self.profilePictureView.frame.size.width/2;
+    self.profilePictureView.clipsToBounds = YES;
+    self.profilePictureView.layer.borderColor = [[UIColor whiteColor] CGColor];
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
